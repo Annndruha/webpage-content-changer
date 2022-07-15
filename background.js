adb5895b60e611a477ce0b4f7fb358ce7d2f7692
Andrey Marakulin <a.p.marakulin@gmail.com>
2022-07-15 03:37:43 +0300

    Update background.js

--- background.js
+++ background.js
@@ -3,27 +3,28 @@
 // }
 
 chrome.tabs.onCreated.addListener(function (tab) {
-    console.log('onCreated')
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: function reddenPage() {
-            //document.body.style.backgroundColor = 'red';
-            console.log('reddenPage')
-        }
+    console.log('onCreated 320723582095-=====')
 
-        //     chrome.tabs.query({
-        //   "currentWindow": true,// Filters tabs in current window
-        //   "status": "complete", //The Page is completely loaded
-        //   "active": true, // The tab or web page is browsed at this state,
-        //   "windowType": "normal", // Filters normal web pages, eliminates g-talk notifications etc
-        // }, function (tabs) {//It returns an array
-        //   for (tab in tabs) {
-        //     $('#url').val(tabs[tab].url);
-        //     $('#title').val(tabs[tab].title);
-        //     $loader.hide();
-        //   }
-        // })
-    })
+    // chrome.scripting.executeScript({
+    //     target: {tabId: tab.id},
+    //     function: function reddenPage() {
+    //         //document.body.style.backgroundColor = 'red';
+    //         console.log('reddenPage')
+    //     }
+    //
+    //     //     chrome.tabs.query({
+    //     //   "currentWindow": true,// Filters tabs in current window
+    //     //   "status": "complete", //The Page is completely loaded
+    //     //   "active": true, // The tab or web page is browsed at this state,
+    //     //   "windowType": "normal", // Filters normal web pages, eliminates g-talk notifications etc
+    //     // }, function (tabs) {//It returns an array
+    //     //   for (tab in tabs) {
+    //     //     $('#url').val(tabs[tab].url);
+    //     //     $('#title').val(tabs[tab].title);
+    //     //     $loader.hide();
+    //     //   }
+    //     // })
+    // })
 })
 // chrome.tabs.onCreated.addListener(
 //     callback: reddenPage
